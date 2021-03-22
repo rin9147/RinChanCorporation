70aef4c1e9a22cdf0b3c320bcf640d9d1e9b9d56
rintaro9147 <rintaroyoshikawa1204@gmail.com>
2021-03-22 14:55:05 +0900

    Add Get METAR. Fix sentence and Interface.

---
+++ script.js
@@ -0,0 +1,9 @@
+function jump() {
+    const URL = "https://tgftp.nws.noaa.gov/data/observations/metar/stations/";
+    icao = document.form.textBox1.value;
+    upper_icao = icao.toUpperCase()
+    //console.log(icao)
+    metar_URL = URL + upper_icao + '.TXT';
+    //metar_URL = "https://tgftp.nws.noaa.gov/data/observations/metar/stations/RJTT.TXT"
+    window.open(metar_URL, '_blank')
+}
