a6e483e5c9b32605d115250c42793b7fbf6d1fa8
rintaro9147 <rintaroyoshikawa1204@gmail.com>
2021-04-08 12:51:47 +0900

    Improve javascript

--- js/temprate.js
+++ js/temprate.js
@@ -1,14 +1,21 @@
-function navbar(){
+function navbar(number){
+    var path;
+    if (number == 1) {
+        path = '../index.html';
+    } else {
+        path = "";
+    }
     var html = "";
     html += '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">';
     html += '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>';
     html += '<div class="collapse navbar-collapse" id="navbarNavAltMarkup"><div class="navbar-nav">';
-    html += '<a class="nav-item nav-link active" href="">Home</a>';
-    html += '<a class="nav-item nav-link" href="#scenery">Scenery</a>';
-    html += '<a class="nav-item nav-link" href="#sector">Sector Files</a>';
-    html += '<a class="nav-item nav-link" href="#metar">Get METAR</a>';
-    html += '<a class="nav-item nav-link" href="#license">License</a>';
-    html += '<a class="nav-item nav-link" href="#contact">Contact</a>';
+    html += '<a class="nav-item nav-link active" href="' + path + '">Home</a>';
+    html += '<a class="nav-item nav-link" href="' + path + '#scenery">Scenery</a>';
+    html += '<a class="nav-item nav-link" href="' + path + '#scenery">Scenery</a>';
+    html += '<a class="nav-item nav-link" href="' + path + '#sector">Sector Files</a>';
+    html += '<a class="nav-item nav-link" href="' + path + '#metar">Get METAR</a>';
+    html += '<a class="nav-item nav-link" href="' + path + '#license">License</a>';
+    html += '<a class="nav-item nav-link" href="' + path + '#contact">Contact</a>';
     html += '</div></div></nav>';
     document.write(html);
 }
